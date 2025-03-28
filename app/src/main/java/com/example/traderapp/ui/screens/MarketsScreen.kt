3902a7249cfdfec2356811e67522a62824f5a38b
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2025-03-28 07:00:41 +0200

    feat(marketScreen): implement search functionality and styling for market screen

--- app/src/main/java/com/example/traderapp/ui/screens/MarketsScreen.kt
+++ app/src/main/java/com/example/traderapp/ui/screens/MarketsScreen.kt
@@ -1,13 +1,19 @@
 package com.example.traderapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.traderapp.R
 import com.example.traderapp.ui.screens.components.CryptoItem
 import com.example.traderapp.ui.screens.components.bars.AppTopBarHome
 import com.example.traderapp.ui.screens.components.bars.BottomNavigationBar
@@ -20,6 +26,7 @@ import com.example.traderapp.viewmodel.CryptoViewModel
 fun MarketScreen(navController: NavController, viewModel: CryptoViewModel) {
     val cryptoList by viewModel.cryptoList.collectAsState()
     val priceUpdates by viewModel.priceUpdates.collectAsState()
+    var searchQuery by remember { mutableStateOf("") }
 
     TransparentStatusBar()
 
@@ -43,12 +50,18 @@ fun MarketScreen(navController: NavController, viewModel: CryptoViewModel) {
                 .padding(paddingValues)
                 .padding(12.dp)
         ) {
+
+            SearchBar(searchQuery) { query -> searchQuery = query }
+
+
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 item {
                     MarketHeader()
                 }
 
-                items(cryptoList) { crypto ->
+                items(cryptoList.filter { crypto ->
+                    crypto.name.contains(searchQuery, ignoreCase = true)
+                }) { crypto ->
                     CryptoItem(
                         crypto = crypto,
                         currentPrice = priceUpdates[crypto.id] ?: crypto.priceUsd.toDouble()
@@ -57,5 +70,36 @@ fun MarketScreen(navController: NavController, viewModel: CryptoViewModel) {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(searchQuery: String, onSearchQueryChanged: (String) -> Unit) {
+    TextField(
+        value = searchQuery,
+        onValueChange = onSearchQueryChanged,
+        label = { Text("Cryptocoin search") },
+        leadingIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.search_icon),
+                contentDescription = "Search",
+                modifier = Modifier.size(30.dp)
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = RoundedCornerShape(16.dp)
+            ),
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        shape = RoundedCornerShape(16.dp)
+    )
 }
 
